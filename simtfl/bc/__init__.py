bce1f0cb065f09e3e3f1a11abb1542eb6d4f18a9
Daira Emma Hopwood <daira@jacaranda.org>
2023-10-19 19:59:37 +0100

    Add tests for simtfl.bc.
    Signed-off-by: Daira Emma Hopwood <daira@jacaranda.org>

--- simtfl/bc/__init__.py
+++ simtfl/bc/__init__.py
@@ -96,3 +96,25 @@ class BCProtocol:
     Transaction: type[object] = BCTransaction
     Context: type[object] = BCContext
     Block: type[object] = BCBlock
+
+
+__all__ = ['BCTransaction', 'BCContext', 'BCBlock', 'BCProtocol']
+
+
+import unittest
+
+
+class TestBC(unittest.TestCase):
+    def test_basic(self):
+        ctx = BCContext()
+        issuance_tx0 = BCTransaction([], [10], 0, issuance=10)
+        assert ctx.add_if_valid(issuance_tx0)
+        genesis = BCBlock(None, 1, [issuance_tx0])
+        assert genesis.score == 1
+
+        issuance_tx1 = BCTransaction([], [6], -1, issuance=5)
+        spend_tx = BCTransaction([issuance_tx0.output(0)], [9], 1)
+        assert ctx.add_if_valid(issuance_tx1)
+        assert ctx.add_if_valid(spend_tx)
+        block1 = BCBlock(genesis, 1, [issuance_tx1, spend_tx])
+        assert block1.score == 2
