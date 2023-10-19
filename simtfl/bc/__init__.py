5b125a80913fb6c4aaca9a6960af443c5d4b45d0
Daira Emma Hopwood <daira@jacaranda.org>
2023-10-19 19:55:23 +0100

    Better handling of coinbase, and add simple block validation.
    Signed-off-by: Daira Emma Hopwood <daira@jacaranda.org>

--- simtfl/bc/__init__.py
+++ simtfl/bc/__init__.py
@@ -16,8 +16,9 @@ class BCTransaction:
         """
         Constructs a `BCTransaction` with the given inputs, output values, fee,
         and (if it is a coinbase transaction) issuance.
-        For a coinbase transaction, pass `fee` as a negative value of magnitude equal
-        to the total amount of fees paid by other transactions in the block.
+        For a coinbase transaction, pass `inputs=[]`, and `fee` as a negative value
+        of magnitude equal to the total amount of fees paid by other transactions
+        in the block.
         """
         assert issuance >= 0
         assert fee >= 0 or len(inputs) == 0
@@ -37,8 +38,8 @@ class BCTransaction:
         return self.outputs[index]
 
     def is_coinbase(self):
-        """Returns `True` if this is a coinbase transaction (it has positive issuance)."""
-        return self.issuance > 0
+        """Returns `True` if this is a coinbase transaction (it has no inputs)."""
+        return len(self.inputs) == 0
 
 
 class BCContext:
@@ -69,16 +70,25 @@ class BCContext:
 
 
 class BCBlock:
-    def __init__(self, parent, added_score, transactions, is_valid=True):
+    def __init__(self, parent, added_score, transactions, allow_invalid=False):
         """
         Constructs a `BCBlock` with the given parent block, score relative to the parent, and transactions.
-        The block need not be valid (since an adversary could provide an invalid block).
+        If `allow_invalid` is set, the block need not be valid.
         Use `parent=None` to construct the genesis block.
         """
         self.parent = parent
         self.score = (0 if parent is None else self.parent.score) + added_score
         self.transactions = transactions
         self.hash = Unique()
+        assert allow_invalid or self.is_valid()
+
+    def is_valid(self):
+        return (
+            len(self.transactions) > 0 and
+            self.transactions[0].is_coinbase() and
+            not any((tx.is_coinbase() for tx in self.transactions[1:])) and
+            sum((tx.fee for tx in self.transactions)) == 0
+        )
 
 
 @dataclass
