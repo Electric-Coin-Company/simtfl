d5c637954d4c1b7bf9b26bf0cfb831acede0754d
Daira Emma Hopwood <daira@jacaranda.org>
2023-10-20 21:11:45 +0100

    Update the simtfl module doc comment to point to tfl-book.
    Co-authored-by: Nathan Wilcox <nathan-at-least@users.noreply.github.com>

--- simtfl/__init__.py
+++ simtfl/__init__.py
@@ -1,6 +1,6 @@
 """
-This is an experimental simulator for research into a potential [Trailing Finality
-Layer](https://electriccoin.co/blog/the-trailing-finality-layer-a-stepping-stone-to-proof-of-stake-in-zcash/)
+This is an experimental simulator for research into a potential
+[Trailing Finality Layer](https://electric-coin-company.github.io/tfl-book/)
 for Zcash.
 
 See the [README](../README.md) for more information.
