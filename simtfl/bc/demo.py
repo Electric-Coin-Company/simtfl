4ad740533aed050952dc03da21f566ef0e9ae0f0
Daira Emma Hopwood <daira@jacaranda.org>
2023-10-21 19:59:04 +0100

    Replace bc-demo with a call to the test, to avoid duplicating code.
    Signed-off-by: Daira Emma Hopwood <daira@jacaranda.org>

--- simtfl/bc/demo.py
+++ simtfl/bc/demo.py
@@ -1,18 +1,8 @@
-from . import BCBlock, BCContext, BCTransaction
+import unittest
+
 
 def run():
     """
     Runs the demo.
     """
-    ctx = BCContext()
-    issuance_tx0 = BCTransaction([], [10], 0, issuance=10)
-    assert ctx.add_if_valid(issuance_tx0)
-    genesis = BCBlock(None, 1, [issuance_tx0])
-    assert genesis.score == 1
-
-    issuance_tx1 = BCTransaction([], [6], -1, issuance=5)
-    spend_tx = BCTransaction([issuance_tx0.output(0)], [9], 1)
-    assert ctx.add_if_valid(issuance_tx1)
-    assert ctx.add_if_valid(spend_tx)
-    block1 = BCBlock(genesis, 1, [issuance_tx1, spend_tx])
-    assert block1.score == 2
+    unittest.main(module='simtfl.bc', verbosity=2)
