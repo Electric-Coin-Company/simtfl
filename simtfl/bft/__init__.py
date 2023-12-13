6bdd41f1c8f561b2a30f68b130bd1b41bae050cd
Daira Emma Hopwood <daira@jacaranda.org>
2023-12-13 17:47:07 +0000

    Re-add __init__.py files.
    Signed-off-by: Daira Emma Hopwood <daira@jacaranda.org>

---
+++ simtfl/bft/__init__.py
@@ -0,0 +1,12 @@
+"""
+Abstractions for Byzantine Fault-Tolerant protocols.
+
+The model of a BFT protocol assumed here was written for the purpose
+of Crosslink, based on the example of adapted-Streamlet ([CS2020] as
+modified in [Crosslink]). It might not be sufficient for other BFT
+protocols — but that's okay; it's a prototype.
+
+[CS2020] https://eprint.iacr.org/2020/088.pdf
+
+[Crosslink] https://hackmd.io/JqENg--qSmyqRt_RqY7Whw?view
+"""
---
+++ simtfl/bft/streamlet/__init__.py
@@ -0,0 +1,3 @@
+"""
+An implementation of adapted-Streamlet ([CS2020] as modified in [Crosslink]).
+"""
